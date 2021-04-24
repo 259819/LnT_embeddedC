264871da531bcd61fd23195f1a03a0d4e648607c
Preet <preetkd2222@gmail.com>
2021-04-24 10:03:44 +0530

    .

--- main.c
+++ main.c
@@ -1,4 +1,12 @@
-/*
+/**
+ * @file Main.c
+ * @author 259819-Preet Kamalnayan Pandit
+ * @brief 
+ * @version 0.1
+ * @date 2021-04-24
+ * 
+ * @copyright Copyright (c) 2021
+ * 
  */
 
 #include <avr/io.h>
@@ -6,22 +14,22 @@
 
 int main(void)
 {
-    DDRB|=(1<<PB0);
-    DDRD&=~(1<<PD0); //CLEAR BIT
-    DDRC&=~(1<<PC0);
-    PORTD|=(1<<PD0);//SET BIT
-    PORTC|=(1<<PC0);
+    DDRB|=(1<<PB0);     //port B pin LED OUTPUT
+    DDRD&=~(1<<PD0);    //CLEAR BIT
+    DDRD&=~(1<<PD1);    //CLEAR BIT
+    PORTD|=(1<<PD0);    //SET BIT -SWITCH-I INPUT
+    PORTD|=(1<<PD1);    //SET BIT -SWITCH-II INPUT
 
 
     for(;;)
     {
-        if(!(PIND&(1<<PD0)) && !(PINC&(1<<PC0)))
+        if(!(PIND&(1<<PD0)) && !(PIND&(1<<PD1)))
         {
-            PORTB|=(1<<PB0);
+            PORTB|=(1<<PB0);    //led on
 
 
         }else{
-            PORTB&=~(1<<PB0);
+            PORTB&=~(1<<PB0);   //led off
         }
     }
 }
